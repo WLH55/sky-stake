393e9f7a755d8c1af9244503d83c2d508a5d4829
陈文龙 <2109236440@qq.com>
2024-08-11 16:04:23 +0800

    微信支付功能开发，伪微信支付功能开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
 
 /**
  * @author CWL
@@ -16,7 +20,36 @@ import org.apache.ibatis.annotations.Mapper;
 public interface OrderMapper {
     /**
      * 插入订单数据
+     *
      * @param order
      */
     void insert(Orders order);
+
+    /**
+     * 根据订单号和用户id查询订单
+     *
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    /**
+     * 修改订单信息
+     *
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 用于替换微信支付更新数据库状态的问题
+     *
+     * @param orderStatus
+     * @param orderPaidStatus
+     */
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus} ,checkout_time = #{check_out_time} " +
+            "where number = #{orderNumber}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, String orderNumber);
+
+
 }
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -30,4 +30,10 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据id查询用户
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 }
