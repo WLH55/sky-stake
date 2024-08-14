15e83df6254e753b7a4a99aa06707b6e4b95fa26
陈文龙 <2109236440@qq.com>
2024-08-14 22:16:20 +0800

    定时任务，websocket用户催单，下单提醒

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Update;
 import org.springframework.core.annotation.Order;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author CWL
@@ -71,4 +72,21 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id = #{id}")
     Orders getById(Long id);
+
+    /**
+     * 根据状态统计订单数量
+     * @param status
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
+
+    /**
+     * 根据状态和下单时间查询订单
+     * @param status
+     * @param orderTime
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
+
+
 }
