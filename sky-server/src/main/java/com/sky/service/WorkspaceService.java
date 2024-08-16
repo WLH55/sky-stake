e61101b5b8d2262cd89005614cdda99033935953
陈文龙 <2109236440@qq.com>
2024-08-16 15:59:22 +0800

    工作台数据概览，数据excel表导出

---
+++ sky-server/src/main/java/com/sky/service/WorkspaceService.java
@@ -0,0 +1,44 @@
+package com.sky.service;
+
+import com.sky.vo.BusinessDataVO;
+import com.sky.vo.DishOverViewVO;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.SetmealOverViewVO;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Classname WorkspaceService
+ * @Description 工作台接口
+ * @Date 2024/8/16 9:07
+ * @Created by cwl
+ */
+public interface WorkspaceService {
+
+    /**
+     * 根据时间段统计营业数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    BusinessDataVO getBusinessData(LocalDateTime begin, LocalDateTime end);
+
+    /**
+     * 查询订单管理数据
+     * @return
+     */
+    OrderOverViewVO getOrderOverView();
+
+    /**
+     * 查询菜品总览
+     * @return
+     */
+    DishOverViewVO getDishOverView();
+
+    /**
+     * 查询套餐总览
+     * @return
+     */
+    SetmealOverViewVO getSetmealOverView();
+
+}
